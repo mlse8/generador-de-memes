e76cfd11d773fcfabba7ff462489a647ab4fa47d
Laura Escalante <laura088.le@gmail.com>
2023-10-17 10:57:49 -0300

    show and hide panel image

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 const $ = (selector) => document.querySelector(selector)
 
 const toggleClass = (selector, className) => $(selector).classList.toggle(className)
+const showPanel = () => $(".panel-container").style.display = "block"
+const hidePanel = () => $(".panel-container").style.display = "none"
+const hideElement = (selector) => $(selector).classList.add("none")
+const showElement = (selector) => $(selector).classList.remove("none")
 
 const lightTheme = () => {
     toggleClass("body", "light-theme")
     toggleClass(".light-off", "none")
     toggleClass(".light-on", "none")
 }
+const showPanelImage = () =>{
+    showPanel()
+    showElement(".panel-image")
+}
+const hidePanelImage = () =>{
+    hidePanel()
+    hideElement(".panel-image")
+}
 
 const initializeProject = () => {
     $('.button-theme').addEventListener('click', () => {
         lightTheme()
+    })
+
+    $(".button-image").addEventListener('click', () => {
+        showPanelImage()
+    })
+
+    $(".panel-button").addEventListener('click', () => {
+        hidePanelImage()
     })
 }
 
