adcfcd67454704813f84ad3641d7cd5da7084e7e
Laura Escalante <laura088.le@gmail.com>
2023-10-18 15:28:48 -0300

    input background-image and fade in animation

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const showPanel = () => $(".panel-container").style.display = "block"
 const hidePanel = () => $(".panel-container").style.display = "none"
 const hideElement = (selector) => $(selector).classList.add("none")
 const showElement = (selector) => $(selector).classList.remove("none")
+const imageBackground = (e) => $(".meme-img").style.backgroundImage = `url("${e.target.value}")`
 
 const lightTheme = () => {
     toggleClass("body", "light-theme")
@@ -23,24 +24,20 @@ const showPanelText = () =>{
 }
 
 const initializeProject = () => {
-    // Modo oscuro / modo claro 
-    $('.button-theme').addEventListener('click', () => {
-        lightTheme()
-    })
+    // Modo oscuro - modo claro 
+    $('.button-theme').addEventListener('click', lightTheme)
 
-    // Abrir y cerrar panel de imagen 
-    $(".button-image").addEventListener('click', () => {
-        showPanelImage()
-    })
+    // Abrir panel de imagen 
+    $(".button-image").addEventListener('click', showPanelImage)
 
-    $(".panel-button").addEventListener('click', () => {
-        hidePanel()
-    })
+    // Abrir  panel de texto
+    $(".button-text").addEventListener('click', showPanelText)
 
-    // Abrir y cerrar panel de texto
-    $(".button-text").addEventListener('click', () => {
-        showPanelText()
-    })
+    //Cerrar panel
+    $(".panel-button").addEventListener('click', hidePanel)
+
+    //Imagen
+    $("#url-input").addEventListener("input", imageBackground)
 }
 
 window.addEventListener("load", initializeProject)
